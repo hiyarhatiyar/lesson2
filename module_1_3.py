927b7cf7a6308b18d07eedc9ff706bee0a4d463e
hiyarhatiyar <GogolMogolkz@gmail.com>
2024-10-27 21:13:37 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Andrey'
+print(name, type(name))
+print('Name: Andrey')
+name = '57'
+print(name, type(name))
+name = 'Age = 57'
+print('Age: 57')
+print('Age: 57 false')
+name = 'New Age = 58'
+print('New Age: 58')
+name = 'Is Student'
+print('Is Student: True')
