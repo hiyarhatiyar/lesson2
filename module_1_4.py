d2b65573c8e7e4fe835af169b9f1efafb8c79ee0
hiyarhatiyar <GogolMogolkz@gmail.com>
2024-11-11 21:13:18 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string = input() # Hello, World!
+print(len(my_string))
+
+name = 'Urban'
+print(name.upper())
+print(name.lower())
+
+print('my string Hello World!'.replace(' ', '#'))
+name = 'Hello, World!'
+print(name[0])
+print(name[-1])
